4fd56d7a7a73dbf4240bf21d58ae8917008d6921
bweigel <benjamin.weigel@europace.de>
2018-07-02 00:53:41 +0200

    rewrite

--- src/main/scala/zippingactor/zippingJob.scala
+++ src/main/scala/zippingactor/zippingJob.scala
@@ -1,5 +1,7 @@
 package zippingactor
 
+import java.util.UUID
+
 import akka.actor.{Actor, ActorLogging, ActorRef, PoisonPill, Props}
 import zippingactor.fileWatcher.{fileIsAvailable, fileIsUnavailable, isFileAvailable}
 import zippingactor.types.S3Location
@@ -8,43 +10,49 @@ import scala.util.Random
 
 object zippingJob {
 
-  def props(jobActorId: String): Props = Props(new zippingJob(jobActorId))
+  def props(jobActorId: String, filesToZip: List[S3Location]): Props = Props(new zippingJob(jobActorId, filesToZip))
 
-  case class zipJobMessage(messageId: String, filesToZip: List[S3Location])
+  case class getJobResult(requestId: String)
 
-  case class getJobResult(messageId: String)
+  case class JobIsDone(requestId: String, s3Location: S3Location)
 
-  case class JobIsDone(messageId: String, s3Location: S3Location)
-
-  case class JobIsPending(messageId: String)
+  case class JobIsPending(requestId: String)
 
 }
 
-class zippingJob(jobActorId: String) extends Actor with ActorLogging {
+class zippingJob(messageId: String, filesToZip: List[S3Location], zipOutputLocation: S3Location) extends Actor with ActorLogging {
 
   import zippingJob._
 
-  var fileWorkers: List[ActorRef] = Nil
+  val fileWorkers: List[ActorRef] = for {
+    file <- filesToZip
+  } yield context.actorOf(fileWatcher.props(messageId, file))
+
+  private var filesProcessed: List[S3Location] = Nil
+
+  def allFilesAvailable: Boolean = (for {
+    a <- filesToZip
+  } yield filesProcessed contains a).reduce(_ && _)
+
   var jobIsDone: Boolean = false
-  var zipOutputLocation: Option[S3Location] = None
 
-  override def preStart(): Unit = log.info(s"zipping job actor $jobActorId started ...")
+  def addAvailableFile(file: S3Location): Unit = {
+    filesProcessed = file :: filesProcessed
+  }
 
-  override def postStop(): Unit = log.info(s"zipping job actor $jobActorId stopped ...")
+  override def preStart(): Unit = log.info(s"zipping job actor for message $messageId started ...")
+
+  override def postStop(): Unit = log.info(s"zipping job actor for message $messageId stopped ...")
 
   override def receive: Receive = {
-    case PoisonPill => log.info("Got Poison Pill...")
-    case zipJobMessage(messageId, content) =>
-      log.info(s"Recieved message '$messageId'")
-      fileWorkers = for {
-        file <- content
-      } yield context.actorOf(fileWatcher.props(messageId, file))
-    case getJobResult(messageId) =>
-     // log.info("Polling files...")
-      fileWorkers.foreach(wrk => wrk ! isFileAvailable(Random.nextString(4)))
-      if(jobIsDone) sender() ! JobIsDone(messageId, zipOutputLocation.get) else sender ! JobIsPending(messageId)
-    case fileIsAvailable(requestId, fileLocation) => log.info(s"new File available $requestId => ${fileLocation.s3Key}")
-    case fileIsUnavailable(requestId) => log.info(s"File not yet available $requestId")
+    case getJobResult(requestId) =>
+      if (!allFilesAvailable) fileWorkers.foreach(wrk => wrk ! isFileAvailable(requestId))
+      if (jobIsDone) sender() ! JobIsDone(requestId, zipOutputLocation) else sender ! JobIsPending(requestId)
+    case fileIsAvailable(requestId, fileLocation) =>
+      log.info(s"new File available $requestId => ${fileLocation.s3Key}")
+      addAvailableFile(fileLocation)
+
+    case fileIsUnavailable(requestId) => log.debug(s"File not yet available $requestId")
 
   }
 
